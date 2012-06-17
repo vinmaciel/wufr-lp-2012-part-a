074c9ea121143aab72408635b5bc6c86d9b812ce
maciel.mvinicius@gmail.com <maciel.mvinicius@gmail.com@3bc9d8c5-563a-6c9e-3a1c-7d0e48b2e8dc>
2012-06-17 17:19:48 +0000

    Stack added (not tested).

---
+++ trunk/PCS2427B/AutomataStack.c
@@ -0,0 +1,70 @@
+/*
+ * AutomataStack.c
+ *
+ *  Created on: 17/06/2012
+ *      Author: Vinicius
+ */
+
+#include <stdlib.h>
+#include "AutomataStack.h"
+
+void newAutomataStack(AutomataStack* stack) {
+	stack = NULL;
+	freeList = NULL;
+}
+
+void getNewStackNode(AutomataStack* newNode) {
+	if(freeList == NULL)
+		*newNode = (AutomataStackNode*) malloc(sizeof(AutomataStackNode));
+	else {
+		*newNode = freeList;
+		freeList = freeList->next;
+	}
+}
+
+void releaseStackNode(AutomataStack freeNode) {
+	freeNode->next = freeList;
+	freeList = freeNode;
+}
+
+void pushAutomaton(AutomataStack* stack, int machine, int nextState) {
+	AutomataStack newNode;
+
+	getNewStackNode(&newNode);
+	newNode->machineIndex = machine;
+	newNode->nextStateIndex = nextState;
+
+	if(stack == NULL) {
+		newNode->next = newNode;
+		*stack = newNode;
+	}
+	else {
+		newNode->next = (*stack)->next;
+		(*stack)->next = newNode;
+	}
+
+	*stack = (*stack)->next;
+}
+
+void popAutomaton(AutomataStack* stack, int* machine, int* nextState) {
+	AutomataStack freeNode;
+
+	freeNode = (*stack)->next;
+
+	*machine = freeNode->machineIndex;
+	*nextState = freeNode->nextStateIndex;
+
+	if(*stack == (*stack)->next)
+		*stack = NULL;
+	else
+		(*stack)->next = freeNode->next;
+
+	releaseStackNode(freeNode);
+}
+
+int isEmptyStack(AutomataStack stack) {
+	if(stack == NULL)
+		return 1;
+
+	return 0;
+}
---
+++ trunk/PCS2427B/AutomataStack.h
@@ -0,0 +1,30 @@
+/*
+ * AutomataStack.h
+ *
+ *  Created on: 17/06/2012
+ *      Author: Vinicius
+ */
+
+#ifndef AUTOMATASTACK_H_
+#define AUTOMATASTACK_H_
+
+#include "Automaton.h"
+
+typedef struct _AutomataStackNode {
+	int machineIndex;
+	int nextStateIndex;
+	struct _AutomataStackNode* next;
+} AutomataStackNode;
+
+typedef AutomataStackNode* AutomataStack;
+
+AutomataStack freeList;
+
+void newAutomataStack(AutomataStack* stack);
+void getNewStackNode(AutomataStack* newNode);
+void releaseStackNode(AutomataStack freeNode);
+void pushAutomaton(AutomataStack* stack, int machine, int nextState);
+void popAutomaton(AutomataStack* stack, int* machine, int* nextState);
+int isEmptyStack(AutomataStack stack);
+
+#endif /* AUTOMATASTACK_H_ */
